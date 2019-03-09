78583bbc93416e62f8b560c246e2995ba57713f5
Maxime Belanger <mbelanger@effenti.ca>
2019-03-09 10:46:55 -0500

    Rename risqc

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to exploi-frontend!');
+    expect(page.getTitleText()).toEqual('Welcome to risqc-frontend!');
   });
 
   afterEach(async () => {
