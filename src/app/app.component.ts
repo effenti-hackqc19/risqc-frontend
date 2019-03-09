78f901104676d77268fa5002c583113555612c35
Maxime Belanger <mbelanger@effenti.ca>
2019-03-09 11:23:35 -0500

    my position

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {LatLng, LatLngLiteral} from '@agm/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  // Map coordinated
   lat = 46.816877;
   lng = -71.200460;
+
+  mapStyles = [
+    {
+      featureType: 'poi',
+      elementType: 'labels',
+      stylers: [
+        { visibility: 'off' }
+      ]
+    }
+  ];
+
+  myPosition: LatLngLiteral = null;
+
+  ngOnInit(): void {
+    navigator.geolocation.getCurrentPosition((position) => {
+      this.myPosition = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      this.lat = position.coords.latitude;
+      this.lng = position.coords.longitude;
+      console.log(this.myPosition);
+    }, (error) => {
+      console.log(error);
+    });
+  }
 }
